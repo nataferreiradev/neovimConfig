3053faf85dd4c8f6b427534fd62cb2948a10a51d
NataNogueiraFerreira <nataferreira929@hotmail.com>
2024-02-17 16:55:15 -0300

    add: fugitive to manage git

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -144,6 +144,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "C:\\Users\\nataf\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
+  },
+  ["vim-fugitive"] = {
+    loaded = true,
+    path = "C:\\Users\\nataf\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\vim-fugitive",
+    url = "https://github.com/tpope/vim-fugitive"
   }
 }
 
